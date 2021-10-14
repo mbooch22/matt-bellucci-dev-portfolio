a842f3e1a3b59386ada01c1daa99b543998b9471
Matthew Bellucci <matthew.bellucci@sap.com>
2021-10-14 16:54:30 -0400

    Updates

---
+++ precache-manifest.45792567861f8e3d11b2d888cea6de71.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f1a145a903c4e5bbfa99676478b9cc",
+    "url": "/matt-bellucci-dev-portfolio/index.html"
+  },
+  {
+    "revision": "800cf1a330f7e3f35476",
+    "url": "/matt-bellucci-dev-portfolio/static/css/2.dfb7c7bb.chunk.css"
+  },
+  {
+    "revision": "58100d977876b7a12161",
+    "url": "/matt-bellucci-dev-portfolio/static/css/main.91ce24e5.chunk.css"
+  },
+  {
+    "revision": "800cf1a330f7e3f35476",
+    "url": "/matt-bellucci-dev-portfolio/static/js/2.e01fe609.chunk.js"
+  },
+  {
+    "revision": "e4126cd9867852e4a884c3a349c37ba5",
+    "url": "/matt-bellucci-dev-portfolio/static/js/2.e01fe609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58100d977876b7a12161",
+    "url": "/matt-bellucci-dev-portfolio/static/js/main.b1b5eadb.chunk.js"
+  },
+  {
+    "revision": "1835f2cee5d464fde8bb",
+    "url": "/matt-bellucci-dev-portfolio/static/js/runtime-main.cbc886c4.js"
+  },
+  {
+    "revision": "ce672761986044bff4ed21ba0b2604a9",
+    "url": "/matt-bellucci-dev-portfolio/static/media/MattBellucciResume.docx.ce672761.pdf"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/matt-bellucci-dev-portfolio/static/media/logo.06e73328.svg"
+  }
+]);
